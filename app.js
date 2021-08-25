6b73e59209ec4b0bbd367cdf3afbcdf7d44b4522
cabernat <cabo81@gmail.com>
2021-08-25 12:09:57 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Buenos dias, Hola Mundo!'));
+app.get('/', (req, res) => res.send('Nuevo cambio de mensaje'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
