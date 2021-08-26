6169d8063b52c368018c5a390135d20313a7df2a
cabernat <cabo81@gmail.com>
2021-08-26 08:43:51 -0500

    Nueva prueba de tubera

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Nuevo cambio de mensaje'));
+app.get('/', (req, res) => res.send('Nuevo cambio de mensaje hoy 26 de agosto de 2021'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
